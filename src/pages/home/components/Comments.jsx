488a13bc5915977e109bf0446cf521cba52d2ccb
toshantarora <toshantarora19@gmai.com>
2023-04-10 21:24:29 +0530

    fix: error in comments

--- src/pages/home/components/Comments.jsx
+++ src/pages/home/components/Comments.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import ShareCommentImage from '../../../assets/images/share-icon.png';
 import { getUserFullName, getUserProfileImage } from '../../../utils/Storage';
 import usePostCommentsById from '../../../hooks/query/Posts/usePostCommentsById';
-import { getInitials, isNonEmptyString } from '../../../helpers';
+import { getInitials, isNonEmptyArray, isNonEmptyString } from '../../../helpers';
 import { useForm } from 'react-hook-form';
 import { useMutation, useQueryClient } from 'react-query';
 import { API } from '../../../services/ApiClient';
@@ -49,98 +49,116 @@ const Comments = ({ postId }) => {
       setValue('comment', '');
     };
 
-  return (
-    <div className="comment-section">
-      {error
-        ? 'Something went wrong'
-        : isLoading
-        ? 'loading'
-        : commentsData
-        ? commentsData?.map((comment, idx) => (
-            <div className="comment-posted">
-              <figure>
-                {comment?.profile_image !== null ? (
-                  <picture>
-                    <source srcSet={comment?.profile_image} type="image/webp" />
-                    <source srcSet={comment?.profile_image} type="image/png" />
-                    <img
-                      loading="lazy"
-                      src="data:image/png;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs="
-                      data-src={comment.profile_image}
-                      alt="user-img"
-                      className="img-fluid"
-                      width={55}
-                      height={55}
-                    />
-                  </picture>
-                ) : (
-                  <span>
-                    {' '}
-                    {isNonEmptyString(comment?.forename) &&
-                    isNonEmptyString(comment?.surname)
-                      ? getInitials(`${comment?.forename}  ${comment?.surname}`)
-                      : ''}
-                  </span>
-                )}
-              </figure>
-              <figcaption>
-                <h6>
-                  <strong>
-                    {isNonEmptyString(comment?.forename) &&
-                    isNonEmptyString(comment?.surname)
-                      ? `${comment?.forename}  ${comment?.surname}`
-                      : 'User'}
-                  </strong>
-                </h6>
-                {isNonEmptyString(comment?.comment) ? (
-                  <p>
-                    {`${comment?.comment.substring(0, MAX_LENGTH)}...`}
+    if(error)
+    {
+      return <div>Something went wrong</div>;
+    }
+    if (isLoading)
+    {
+      return <div>Loading...</div>;
+    }
+      return (
+        <div className="comment-section">
+          {
+          isNonEmptyArray(commentsData)
+            ? commentsData?.map((comment, idx) => (
+                <div key={idx} className="comment-posted">
+                  <figure>
+                    {comment?.profile_image !== null ? (
+                      <picture>
+                        <source
+                          srcSet={comment?.profile_image}
+                          type="image/webp"
+                        />
+                        <source
+                          srcSet={comment?.profile_image}
+                          type="image/png"
+                        />
+                        <img
+                          loading="lazy"
+                          src="data:image/png;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs="
+                          data-src={comment.profile_image}
+                          alt="user-img"
+                          className="img-fluid"
+                          width={55}
+                          height={55}
+                        />
+                      </picture>
+                    ) : (
+                      <span>
+                        {' '}
+                        {isNonEmptyString(comment?.forename) &&
+                        isNonEmptyString(comment?.surname)
+                          ? getInitials(
+                              `${comment?.forename}  ${comment?.surname}`
+                            )
+                          : ''}
+                      </span>
+                    )}
+                  </figure>
+                  <figcaption>
+                    <h6>
+                      <strong>
+                        {isNonEmptyString(comment?.forename) &&
+                        isNonEmptyString(comment?.surname)
+                          ? `${comment?.forename}  ${comment?.surname}`
+                          : 'User'}
+                      </strong>
+                    </h6>
+                    {isNonEmptyString(comment?.comment) ? (
+                      <p>
+                        {`${comment?.comment.substring(0, MAX_LENGTH)}...`}
 
-                    <a href="/">See More</a>
-                  </p>
-                ) : (
-                  'No comment'
-                )}
-              </figcaption>
-            </div>
-          ))
-        : ''}
-      <div className="share-comment">
-        <figure>
-          {userProfilePic !== null ? (
-            <picture>
-              <source srcSet={userProfilePic} type="image/webp" />
-              <source srcSet={userProfilePic} type="image/png" />
-              <img
-                loading="lazy"
-                src="data:image/png;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs="
-                data-src={userProfilePic}
-                alt="user-img"
-                className="img-fluid"
-                width={50}
-                height={50}
+                        <a href="/">See More</a>
+                      </p>
+                    ) : (
+                      'No comment'
+                    )}
+                  </figcaption>
+                </div>
+              ))
+            : ''}
+          <div className="share-comment">
+            <figure>
+              {userProfilePic !== null ? (
+                <picture>
+                  <source srcSet={userProfilePic} type="image/webp" />
+                  <source srcSet={userProfilePic} type="image/png" />
+                  <img
+                    loading="lazy"
+                    src="data:image/png;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs="
+                    data-src={userProfilePic}
+                    alt="user-img"
+                    className="img-fluid"
+                    width={50}
+                    height={50}
+                  />
+                </picture>
+              ) : (
+                <span className="text-uppercase  text-white">
+                  {userProfileText}
+                </span>
+              )}
+            </figure>
+            <span>
+              <input
+                type="text"
+                name="comment"
+                {...register('comment')}
+                placeholder="Write your comment...."
               />
-            </picture>
-          ) : (
-            <span className="text-uppercase  text-white">
-              {userProfileText}
+              <button type="button" onClick={handleSubmit(onSubmit)}>
+                <img
+                  src={ShareCommentImage}
+                  alt="share"
+                  width={24}
+                  height={24}
+                />
+              </button>
             </span>
-          )}
-        </figure>
-        <span>
-          <input
-            type="text"
-            name="comment"
-            {...register('comment')}
-            placeholder="Write your comment...."
-          />
-          <button type="button" onClick={handleSubmit(onSubmit)}>
-            <img src={ShareCommentImage} alt="share" width={24} height={24} />
-          </button>
-        </span>
-      </div>
-    </div>
-  );
+          </div>
+        </div>
+      );
 };
 
 export default Comments;
