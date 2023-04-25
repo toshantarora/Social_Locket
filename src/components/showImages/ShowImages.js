da6ec948224860aa3878ff60e643ab7820c0aef4
toshantarora <toshantarora19@gmai.com>
2023-04-25 15:55:59 +0530

    image uploads on post

--- src/components/showImages/ShowImages.js
+++ src/components/showImages/ShowImages.js
@@ -4,18 +4,28 @@ import './showImagesStyles.css';
 const Image = ({ image }) => {
   return (
     <div>
-      <img className="showImage" alt="" src={image.preview} />
+      <img className="showImage" alt="" src={image} />
     </div>
   );
 };
 
 // export default Image;
-const ShowImage = ({ images }) => {
+const ShowImage = ({ images, uploadStatus }) => {
   const show = (image) => {
     return <Image image={image} />;
   };
 
-  return <div className="show">{images.map(show)}</div>;
+  return (
+    <div className="show">
+      {images.length > 0 ? (
+        images.map(show)
+      ) : (
+        <p>Drag and drop some files here, or click to select files</p>
+      )}
+      {uploadStatus === 'success' && <p>Images uploaded successfully!</p>}
+      {uploadStatus === 'error' && <p>Error uploading images!</p>}
+    </div>
+  );
 };
 
 export default ShowImage;
